95cdc6f6269f4bf9978aa11d396e93e221ef8182
MAO <lcdsmao@gmail.com>
2017-07-17 21:49:13 +0900

    Update View holder and adapter

--- app/src/main/java/com/example/mao/bbc6minuteenglish/BBCContentAdapter.java
+++ app/src/main/java/com/example/mao/bbc6minuteenglish/BBCContentAdapter.java
@@ -1,10 +1,16 @@
 package com.example.mao.bbc6minuteenglish;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.mao.bbc6minuteenglish.data.BBCContentContract;
+import com.example.mao.bbc6minuteenglish.data.BBCContentContract.BBC6MinuteEnglishEntry;
 
 /**
  * Created by MAO on 7/17/2017.
@@ -13,8 +19,11 @@ import android.view.ViewGroup;
 public class BBCContentAdapter extends
         RecyclerView.Adapter<BBCContentAdapter.BBCContentAdapterViewHolder> {
 
-    public BBCContentAdapter() {
+    private Cursor mCursor;
+    private Context mContext;
 
+    public BBCContentAdapter(Context context) {
+        mContext = context;
     }
 
     @Override
@@ -29,11 +38,34 @@ public class BBCContentAdapter extends
     @Override
     public void onBindViewHolder(BBCContentAdapterViewHolder holder, int position) {
 
+        int titleIndex = mCursor.getColumnIndex(BBC6MinuteEnglishEntry.COLUMN_TITLE);
+        int timeIndex = mCursor.getColumnIndex(BBC6MinuteEnglishEntry.COLUMN_TIME);
+        int desIndex = mCursor.getColumnIndex(BBC6MinuteEnglishEntry.COLUMN_DESCRIPTION);
+        int imgIndex = mCursor.getColumnIndex(BBC6MinuteEnglishEntry.COLUMN_THUMBNAIL);
+
+        // Set cursor to position
+        mCursor.moveToPosition(position);
+
+        // Title set
+        holder.mTitleTextView.setText(mCursor.getString(titleIndex));
+
+        // Time set
+        holder.mTimeTextView.setText(mCursor.getString(timeIndex));
+
+        // Description set
+        holder.mDescriptionTextView.setText(mCursor.getString(desIndex));
+
+        // Thumbnail Set
+        // For test, use local img
+        holder.mThumbnailImageView.setImageResource(R.drawable.example);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        if (mCursor == null) {
+            return 0;
+        }
+        return mCursor.getCount();
     }
 
     /**
@@ -41,8 +73,17 @@ public class BBCContentAdapter extends
      */
     public class BBCContentAdapterViewHolder extends RecyclerView.ViewHolder {
 
+        ImageView mThumbnailImageView;
+        TextView mTitleTextView;
+        TextView mTimeTextView;
+        TextView mDescriptionTextView;
+
         public BBCContentAdapterViewHolder (View view) {
             super(view);
+            mThumbnailImageView = (ImageView) view.findViewById(R.id.iv_thumbnail);
+            mTitleTextView = (TextView) view.findViewById(R.id.tv_title);
+            mTimeTextView = (TextView) view.findViewById(R.id.tv_time);
+            mDescriptionTextView = (TextView) view.findViewById(R.id.tv_description);
         }
     }
 }
