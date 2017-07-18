48c53e802f52c21bd4d24fdc03ea28eb9efe8522
MAO <lcdsmao@gmail.com>
2017-07-18 21:29:05 +0900

    Let the database store less quality bitmap

--- app/src/main/java/com/example/mao/bbc6minuteenglish/utilities/DbBitmapUtility.java
+++ app/src/main/java/com/example/mao/bbc6minuteenglish/utilities/DbBitmapUtility.java
@@ -17,7 +17,7 @@ public class DbBitmapUtility {
     // convert from bitmap to byte array
     public static byte[] getBytes(Bitmap bitmap) {
         ByteArrayOutputStream stream = new ByteArrayOutputStream();
-        bitmap.compress(Bitmap.CompressFormat.PNG, 0, stream);
+        bitmap.compress(Bitmap.CompressFormat.WEBP, 30, stream);
         return stream.toByteArray();
     }
 
