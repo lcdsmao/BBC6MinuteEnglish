adc60af891c279cafcf599ce08161e23da130fc3
MAO <lcdsmao@gmail.com>
2017-08-01 22:30:47 +0900

    Add licenses file

--- app/src/main/java/com/example/mao/bbc6minuteenglish/SettingFragment.java
+++ app/src/main/java/com/example/mao/bbc6minuteenglish/SettingFragment.java
@@ -1,11 +1,16 @@
 package com.example.mao.bbc6minuteenglish;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceScreen;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.webkit.WebView;
 
 import com.example.mao.bbc6minuteenglish.sync.BBCSyncUtility;
 
@@ -19,6 +24,7 @@ public class SettingFragment extends PreferenceFragment
         addPreferencesFromResource(R.xml.setting_preference);
 
         setMaxHistorySummary();
+        setPreferenceClickListener();
     }
 
     @Override
@@ -34,6 +40,31 @@ public class SettingFragment extends PreferenceFragment
         PreferenceScreen preferenceScreen = getPreferenceScreen();
         ListPreference preference = (ListPreference) preferenceScreen.findPreference(key);
         preference.setSummary(preference.getValue());
+    }
+
+    private void displayLicensesAlertDialog() {
+        WebView view = (WebView) LayoutInflater.from(getActivity())
+                .inflate(R.layout.dialog_licenses, null);
+        view.loadUrl("file:///android_asset/Licenses.html");
+        new AlertDialog.Builder(getActivity(), R.style.Theme_AppCompat_Light_Dialog_Alert)
+                .setTitle(getString(R.string.open_source_license))
+                .setView(view)
+                .setPositiveButton(android.R.string.ok, null)
+                .show();
+    }
+
+    private void setPreferenceClickListener() {
+        Context context = getActivity();
+        PreferenceScreen preferenceScreen = getPreferenceScreen();
+        Preference licensesPreference =
+                preferenceScreen.findPreference(getString(R.string.setting_licenses_key));
+        licensesPreference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                displayLicensesAlertDialog();
+                return true;
+            }
+        });
     }
 
     @Override
